d3cdbfa94bf2c06d9a20540b64efd76449fd5765
anshjain16 <anshjain0216@gmail.com>
2024-04-07 03:07:33 +0530

    handling file uploads

--- middleware/speechToText.js
+++ middleware/speechToText.js
@@ -1,11 +1,23 @@
 import fs from "fs";
 import OpenAI from "openai";
+import Download from "node-remote-file-save";
 
 const openai = new OpenAI();
 
+let res1 = Download.download({
+  url: "https://voiceage.com/wbsamples/in_mono/Conference.wav",
+  dir: "../uploads",
+});
+
+let recordingPath;
+await res1.then((res1) => {
+  console.log(res1);
+  recordingPath = res1.data.filePath;
+});
+
 async function speechToText(req, res, next) {
   const transcription = await openai.audio.transcriptions.create({
-    file: fs.createReadStream("./test-audio1.mp3"),
+    file: fs.createReadStream(recordingPath),
     model: "whisper-1",
   });
 
